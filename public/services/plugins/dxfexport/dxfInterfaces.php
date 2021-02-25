5eb1daa560eb76e7754af38b4b64e248802ebbbf
Filippo Formentini <formentini@perspectiva.it>
2021-02-25 17:32:50 +0100

    Definito lo spessore aumentato per le guaine

--- public/services/plugins/dxfexport/dxfInterfaces.php
+++ public/services/plugins/dxfexport/dxfInterfaces.php
@@ -18,7 +18,7 @@ interface iDxfCode
     public function addLayer($layerName, $aciColor, $color, $lineType);
 	public function addPoint3D($layerName, $x, $y, $z, $thickness, $color);
 	public function addPolyLine3d($layerName, $coords, $thickness, $lineType, $color);
-	public function addPolyLine($layerName, $coords, $thickness, $lineType, $color);
+	public function addPolyLine($layerName, $coords, $thickness, $lineType, $color, $lineWeigth);
 	public function addPolygon($layerName, $coords, $thickness, $lineType, $outlineColor, $color);
 	public function addHatch($layerName, $coords, $color, $pattern, $parentHandle);
 	public function addText($layerName, $x, $y, $z, $text, $labelSize, $angle, $textAlignHorizontal, $textAlignVertical, $color);
